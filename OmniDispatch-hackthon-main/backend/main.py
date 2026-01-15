ee15cf778a6543ca2a171113667dcf0e7e6d5422
chindukuru naga sai sreedhar <61618600+saisreedhar2001@users.noreply.github.com>
2026-01-15 20:10:04 +0530

    backend_cors

--- OmniDispatch-hackthon-main/backend/main.py
+++ OmniDispatch-hackthon-main/backend/main.py
@@ -30,7 +30,7 @@ app = FastAPI(
 # CORS Configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000","https://omni-dispatch-hackthon-main.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
